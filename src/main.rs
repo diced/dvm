747994cbbb00eced0e4167b94949c7c82ab008f0
diced <pranaco2@gmail.com>
2021-05-22 17:18:39 -0700

    many aliases and possible values

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use dvm::{Res, error, r#type::Type, install::install, remove::remove, update::update, show::show};
 use clap::{AppSettings, Clap};
+
+const POSSIBLE_VALUES: &[&str] = &["stable", "discord-stable", "s", "canary", "discord-canary", "c", "ptb", "discord-ptb", "p", "development", "dev", "discord-development", "d"];
+
 #[derive(Clap, Debug)]
-#[clap(version = "0.1.1")]
-#[clap(setting = AppSettings::ColoredHelp)]
+#[clap(version = "0.1.1", setting = AppSettings::ColoredHelp)]
 struct Opts {
   #[clap(subcommand)]
   command: Command,
@@ -10,31 +12,34 @@ struct Opts {
 
 #[derive(Clap, Debug)]
 enum Command {
-  #[clap(about = "install the latest <type> of discord")]
+  #[clap(about = "install the latest <type> of discord", aliases = &["i", "in", "get"])]
   Install(InstallOption),
 
-  #[clap(about = "update to the latest <type> of discord")]
+  #[clap(about = "update to the latest <type> of discord", aliases = &["u", "up", "upgrade"])]
   Update(UpdateOption),
 
-  #[clap(about = "remove the installed <type> of discord")]
+  #[clap(about = "remove the installed <type> of discord", aliases = &["r", "rm", "d", "del", "un", "uninstall"])]
   Remove(RemoveOption),
 
-  #[clap(about = "show all installed versions")]
+  #[clap(about = "show all installed versions", aliases = &["s", "installed", "all", "versions", "types"])]
   Show
 }
 
 #[derive(Clap, Debug)]
 struct InstallOption {
+  #[clap(possible_values = POSSIBLE_VALUES)]
   r#type: String
 }
 
 #[derive(Clap, Debug)]
 struct UpdateOption {
+  #[clap(possible_values = POSSIBLE_VALUES)]
   r#type: String
 }
 
 #[derive(Clap, Debug)]
 struct RemoveOption {
+  #[clap(possible_values = POSSIBLE_VALUES)]
   r#type: String
 }
 
@@ -50,6 +55,7 @@ fn str_to_type(s: String) -> Type {
     }
   }
 }
+
 
 #[tokio::main]
 async fn main() -> Res<()> {
